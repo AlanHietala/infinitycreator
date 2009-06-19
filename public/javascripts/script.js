1de26112c30e5179866c38df3752061b1a6e1fbc
Alan Hietala <alanhietala@alan-hietalas-macbook-pro.local>
2009-06-19 09:32:28 -0400

    UI enhancements

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,57 @@
 $(document).ready(function(){
-	$(".expand").click(function(){
-		$(this).parents(".unit").find(".unitdetails").toggle("scale");
+	var clickareas = $(".unitheader");
+	clickareas.bind("click",function(e){
+		$(e.target).parents("actions")
+		var unit = $(this).parents(".unit");
+		var unitdetails = unit.find(".unitdetails");
+		var imagehandle = unit.find(".unitheader .expand img");
+		unitdetails.toggle("scale");
+				
+		var img = "";
+		
+		if(imagehandle.attr("src").indexOf("minus")>0){
+			img = "/images/plushover.png";
+
+		}else{
+			img ="/images/minushover.png";
+
+		}
+		imagehandle.attr("src",img);
+			
+	
+	
 		return false;
 	});
+	$(".actions").bind("click",function(e){
+	
+		e.stopPropagation;
+	});
+	clickareas.bind("mouseover",function(e){
+		var img = "";
+		var unit = $(this).parents(".unit");
+			if(unit.find(".unitdetails").css("display")=="none"){
+				img = "/images/plushover.png";
+				
+			}else{
+				img = "/images/minushover.png";
+				
+			}
+			unit.find(".unitheader .expand img").attr("src",img);
+			unit.find(".unitheader").addClass("active");
+	});
+	clickareas.bind("mouseout",function(e){
+		var img = ""
+		var unit = $(this).parents(".unit");
+			if($(this).parents(".unit").find(".unitdetails").css("display")=="none"){
+				img = "/images/plus.png";
+				
+			}else{
+				img = "/images/minus.png";
+				
+			}
+			unit.find(".unitheader .expand img").attr("src",img);
+			unit.find(".unitheader").removeClass("active");
+	});
+	
 });
+
