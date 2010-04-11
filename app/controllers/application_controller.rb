9fec4110ab0a464137d6bfb0be49999cfade7798
Alan Hietala <alanhietala@alan-hietalas-macbook-pro.local>
2010-04-11 18:41:11 -0400

    added mobile site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,11 @@ class ApplicationController < ActionController::Base
   # filter_parameter_logging :password
   
   def adjust_format_for_mobile
-    # if (request.subdomains.first == "m")
-    #       request.format = :mobile
-    #     end
+    if request.subdomains.first == "m" || 
+       (RAILS_ENV != "production" && 
+        request.env["HTTP_USER_AGENT"] && 
+        request.env["HTTP_USER_AGENT"][/(iPhone|iPod)/])
+      request.format = :mobile
+    end
   end
 end
